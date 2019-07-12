c6591c6432332c22139f548057aa50e929716ed7
allisonbusse <allisonbusse@gmail.com>
2019-07-11 21:02:24 -0700

    implemented function

---
+++ app.js
@@ -0,0 +1,22 @@
+import isEven from './is-even.js';
+
+// reference elements
+const yourNumber = document.getElementById('yourNumber');
+const button = document.getElementById('button');
+const even = document.getElementById('even');
+const odd = document.getElementById('odd');
+
+// wire up events
+button.onclick = function() {
+    const x = Number(yourNumber.value);
+    console.log('hi');
+    if(isEven(x)) {
+        odd.classList.add('hidden');
+        even.classList.remove('hidden');
+    } else {
+        even.classList.add('hidden');
+        odd.classList.remove('hidden');
+    }
+    
+
+}
